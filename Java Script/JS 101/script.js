84357f5f842662411e4b38701a614a4fa8e491c1
Nischal <burninglykfire@gmail.com>
2022-12-12 10:45:49 +0545

    Intro to Basic JS

---
+++ Java Script/JS 101/script.js
@@ -0,0 +1,28 @@
+// var website = "https://facebook.com/";
+// var Name = "Nischal Maharjan";
+
+// if (Name == "Nischal Maharjan") {
+//   let apples = 10;
+//   const mango = 5;
+//   console.log("I have", apples, "apples");
+//   console.log("I have", mango, "mangoes");
+// }
+
+// function addNumber(Name, ...numbers) {
+//   let total = 0;
+//   for (index in numbers) {
+//     total = total + numbers[index];
+//   }
+//   return `"${Name}:The total of numbers is ${total}"`;
+// }
+
+// const addedNumber = addNumber("Nischal", 1, 5, 67, 4);
+
+const person = {
+  Name: "Nischal",
+  age: 21,
+  height: "5'9\"",
+  speak: function (want = "abisekh") {
+    console.log(`I want to speak with ${want}`);
+  },
+};
